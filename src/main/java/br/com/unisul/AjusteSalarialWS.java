38a1213f550d1ff7c39cff5739ab42bc8758d275
Luiz Alves <luizhmalves@hotmail.com>
2020-08-01 10:49:42 -0300

    Commit inicial
    Commit inicial

---
+++ src/main/java/br/com/unisul/AjusteSalarialWS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.unisul;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Luiz
+ */
+@WebService(serviceName = "AjusteSalarialWS")
+@Stateless()
+public class AjusteSalarialWS {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        int k = i + j;
+        return k;
+    }
+}
