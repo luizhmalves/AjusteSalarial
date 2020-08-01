47181de04769964543fd52d280c975614c78423f
Luiz Alves <luizhmalves@hotmail.com>
2020-08-01 17:37:04 -0300

    Retirada de classes desnecessarias
    Retirada de classes desnecessarias

--- src/main/java/br/com/unisul/AjusteSalarialWS.java
+++ src/main/java/br/com/unisul/AjusteSalarialWS.java
@@ -1,46 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.unisul;
 
-import br.com.unisul.ajustesalarialws.definicoes.ISalarialService;
-import br.com.unisul.ajustesalarialws.request.AjusteSalarialRequest;
 import br.com.unisul.ajustesalarialws.response.AjusteSalarialResponse;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.ejb.Stateless;
+import javax.xml.bind.annotation.XmlElement;
 
 /**
  *
  * @author Luiz
  */
-@WebService(serviceName = "AjusteSalarialWS", endpointInterface = "br.com.unisul.ajustesalarialws.definicoes.ISalarialService")
+@WebService(serviceName = "AjusteSalarialWS")
 @Stateless()
-public class AjusteSalarialWS implements ISalarialService{
-    
-    
-    @Override
-    public AjusteSalarialResponse CalcularAjusteSalarial(AjusteSalarialRequest request) {
-        
+public class AjusteSalarialWS{
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "calcularAjusteSalarial")
+    public AjusteSalarialResponse calcularAjusteSalarial(
+            @WebParam(name = "nomeFuncionario") String nomeFuncionario, 
+            @WebParam(name = "salarioAtual") Double salarioAtual, 
+            @WebParam(name = "percentualAumento") Double percentualAumento) {
         AjusteSalarialResponse response = new AjusteSalarialResponse();
         
         //CALCULANDO O PERCENTUAL DE AJUSTE
         
-        Double ajusteSalario = ((request.getSalarioAtual() / 100) * request.getPercentualAumento());
+       Double ajusteSalario = ((salarioAtual / 100) * percentualAumento);
         
         //SOMA O AJUSTE MAIS O SALÁRIO ATUAL
+       
+        Double novoSalario = ajusteSalario + salarioAtual;
         
-        Double novoSalario = ajusteSalario + request.getSalarioAtual();
-        
-        response.setNomeFuncionario(request.getNomeFuncionario());
-        response.setPercentualAumentoRecebido(request.getPercentualAumento());
+        response.setNomeFuncionario(nomeFuncionario);
+        response.setPercentualAumentoRecebido(percentualAumento);
         response.setSalarioAjustado(novoSalario);
-        response.setSalarioAnterior(request.getSalarioAtual());
+        response.setSalarioAnterior(salarioAtual);
         
         return response;
-        
     }
 }
